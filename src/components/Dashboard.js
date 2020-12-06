a1781279c32bf5868bda4c19f962900bb2d7f9b2
RajPathare <rajgpathare@gmail.com>
2020-12-06 12:04:58 +0530

    added logout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+import history from '../history';
+
+
 class Dashboard extends React.Component {
 
+
+    logoutFunc = () => {
+        console.log('logout invoked!');
+        localStorage.removeItem('isSignedIn');
+        history.push('/login');
+
+    }
 
     renderComponent = () => {
 
         return (
-            <div>Dashboard!</div>
+            <div>
+                <h1>Dashboard!</h1>
+                <button className="ui button primary" onClick={()=> this.logoutFunc()}>Logout</button>
+            </div>
         )
     }
 
